1d2071ac2d1c10c0717f1617fccd55d7b2ee6a62
bozorg <bozorg.shkn6@gmail.com>
2023-08-21 13:20:43 +0500

    assignment 17

--- members/models.py
+++ members/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  football_quiz = models.CharField(max_length=255)
+  general_quiz = models.CharField(max_length=255)
--- members/views.py
+++ members/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("hello people ")
-
-# Create your views here.
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
