62a95c4f769087a48457ce7c9ed08af4f34e940d
sonira <sonira.shokohyan531@gmail.com>
2023-08-25 12:29:41 +0500

    fast_quiz

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='members'),
+    # path('', views.index, name='index'),
+    path('members/', views.members, name='members'),
+    path('members/index', views.index, name='index'),
+    path('members/index/football', views.football, name='football'),
+    path('members/index/general', views.general, name='general'),
+    path('members/index/contact', views.contact, name='contact'),
+    path('members/index/about', views.about, name='about'),
+    path('members/details/<int:id>', views.details, name='details'),
+
 ]
--- members/views.py
+++ members/views.py
@@ -1,7 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
+
 
 def members(request):
+    mymembers = Member.objects.all().values()                # data ra az databse megira
+    template = loader.get_template('all_members.html')   
+    context = {
+    'mymembers': mymembers,
+  }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+
+
+
+def index(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+
+def football(request):
+    template = loader.get_template('football.html')
+    return HttpResponse(template.render())
+
+def general(request):
+    template = loader.get_template('general.html')
+    return HttpResponse(template.render())
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
